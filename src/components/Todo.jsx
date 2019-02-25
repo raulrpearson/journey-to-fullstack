b2298e00495b741cad23efbc3c2a8502f108d5f8
Raúl R. Pearson <raulrpearson@protonmail.com>
2019-02-25 12:27:36 +0700

    Add Todo component

---
+++ src/components/Todo.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const TodoBox = styled.div`
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 1em;
+  margin: 2em 0;
+
+  p {
+    margin: 0;
+  }
+
+  span.todo {
+    color: tomato;
+    margin-right: 1em;
+    font-weight: bold;
+  }
+`;
+
+const Todo = ({ children }) => (
+  <TodoBox>
+    <p>
+      <span class="todo">TODO</span>
+      {children}
+    </p>
+  </TodoBox>
+);
+
+export default Todo;
